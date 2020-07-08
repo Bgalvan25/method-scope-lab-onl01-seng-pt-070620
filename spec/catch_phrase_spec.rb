9d97071b535f92049edc1646cc72b3c0e9f7e8c4
Brittney Galvan <brittney.galvan25@yahoo.com>
2020-07-08 16:41:25 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 def catch_phrase(phrase)
     puts phrase = "It's-a me, Mario!"
-    return "#{phrase}".to output("It's-a me, Mario!\n").to_stdout
+    expect"#{phrase}".to output("It's-a me, Mario!\n").to_stdout
   end
 end
